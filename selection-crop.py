e490d24e846210f31dbb35eee755e43d3bfb7840
Dominik <01625726@student.tuwien.ac.at>
2020-02-10 21:52:47 +0100

    fixed error when nothing is selected

--- selection-crop.py
+++ selection-crop.py
@@ -18,11 +18,11 @@ def cropSelection(image):
         f.write('%s\t%d\t%d\t%d\t%d\n' % (name,x1,y1,x2-x1,y2-y1))
         f.close()    
         image.resize(x2-x1, y2-y1, -x1, -y1)
-    pdb.gimp_displays_flush()
-    layer = pdb.gimp_image_merge_visible_layers(image, CLIP_TO_IMAGE)
-    pdb.gimp_file_save(image, layer, image.filename, image.filename)
-    display=gimp._id2display(image.ID)
-    pdb.gimp_display_delete(display)
+        pdb.gimp_displays_flush()
+        layer = pdb.gimp_image_merge_visible_layers(image, CLIP_TO_IMAGE)
+        pdb.gimp_file_save(image, layer, image.filename, image.filename)
+        display=gimp._id2display(image.ID)
+        pdb.gimp_display_delete(display)
 
 
 ### Registration
